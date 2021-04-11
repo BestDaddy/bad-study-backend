ec84f1a09fc109926c5ac4b852f66dda843266c1
BestDaddy <azik.99.kz@gmail.com>
2021-04-11 22:08:02 +0600

    test

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -13,8 +13,8 @@
 
     <title>SB Admin 2</title>
 
-    <script src="{{asset('vendor/jquery/jquery.min.js')}}"></script>
-{{--    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>--}}
+{{--    <script src="{{asset('vendor/jquery/jquery.min.js')}}"></script>--}}
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 {{--    <script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>--}}
 {{--    <script src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>--}}
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
