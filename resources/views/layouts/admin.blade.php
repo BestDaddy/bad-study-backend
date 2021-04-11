ca4c587d2aba5b98567731403f13cce2165d61fe
BestDaddy <azik.99.kz@gmail.com>
2021-04-11 22:40:52 +0600

    fix

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -13,29 +13,21 @@
 
     <title>SB Admin 2</title>
 
-{{--    <script src="{{asset('vendor/jquery/jquery.min.js')}}"></script>--}}
-{{--    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>--}}
-{{--    <script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>--}}
-{{--    <script src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>--}}
-{{--    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>--}}
 
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js"></script>
-    <script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
-    <script src="https://cdn.datatables.net/1.10.21/js/dataTables.bootstrap4.min.js"></script>
     <!-- Custom fonts for this template -->
     <link rel="stylesheet" type="text/css" href="{{ URL::asset('vendor/fontawesome-free/css/all.min.css') }}" />
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-
     <!-- Custom styles for this template -->
     <link rel="stylesheet" href="{{ URL::asset('css/sb-admin-2.min.css') }}" />
 
 
-    <!-- Custom styles for this page -->
-    <link href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css" rel="stylesheet">
-    <link href="https://cdn.datatables.net/1.10.21/css/dataTables.bootstrap4.min.css" rel="stylesheet">
+    <link href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css" rel="stylesheet">
+    <link href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js"></script>
+    <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
 
 {{--    <link rel="stylesheet" href="{{ URL::asset('vendor/datatables/dataTables.bootstrap4.min.css') }}" />--}}
     @yield('header')
@@ -104,22 +96,12 @@
         <!-- Nav Item - Pages Collapse Menu -->
         <li class="nav-item">
             <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-{{--                <i class="fas fa-fw fa-cog"></i>--}}
                 <i class="fas fa-fw fa-folder"></i>
                 <span>Links</span>
             </a>
             <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                 <div class="bg-white py-2 collapse-inner rounded">
-{{--                    @if(Auth::user()->role_id == 1)--}}
-{{--                    <h6 class="collapse-header">Admin links:</h6>--}}
-{{--                    <a class="collapse-item" href="{{route('users.index')}}">Courses</a>--}}
-{{--                    <a class="collapse-item" href="{{route('roads.index')}}">Roads</a>--}}
-{{--                    <a class="collapse-item" href="{{route('users.index')}}">Users</a>--}}
-{{--                    @endif--}}
-{{--                    <h6 class="collapse-header">Custom links:</h6>--}}
-{{--                    <a class="collapse-item" href="{{route('cars.index')}}">My Cars</a>--}}
-{{--                    <a class="collapse-item" href="{{route('myCourses')}}">My courses</a>--}}
-{{--                    <a class="collapse-item" href="{{route('myGroups')}}">My groups</a>--}}
+
                 </div>
             </div>
         </li>
@@ -272,18 +254,19 @@
     </div>
 </div>
 @yield('scripts')
+
 <!-- Bootstrap core JavaScript-->
 <!-- Bootstrap core JavaScript-->
 
-<script src="{{asset('vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+{{--<script src="{{asset('vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>--}}
 
 
-<!-- Core plugin JavaScript-->
-<script src="{{asset('vendor/jquery-easing/jquery.easing.min.js')}}"></script>
+{{--<!-- Core plugin JavaScript-->--}}
+{{--<script src="{{asset('vendor/jquery-easing/jquery.easing.min.js')}}"></script>--}}
 
 
-<!-- Custom scripts for all pages-->
-<script src="{{asset('js/sb-admin-2.min.js')}}"></script>
+{{--<!-- Custom scripts for all pages-->--}}
+{{--<script src="{{asset('js/sb-admin-2.min.js')}}"></script>--}}
 
 
 <!-- Page level plugins -->
