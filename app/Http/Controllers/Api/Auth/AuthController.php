a9759166681516a19edb5a100badaf4f6745efac
BestDaddy <azik.99.kz@gmail.com>
2021-04-12 22:47:25 +0600

    fix

--- app/Http/Controllers/Api/Auth/AuthController.php
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -16,16 +16,16 @@ class AuthController extends ApiBaseController
     public function authenticate(LoginApiRequest $request)
     {
 //        $cred = collect($request)->only(['phone', 'email'])->toArray();
-        $user = User::with('role')
-            ->where('email', $request->email)
-            ->first();
+//        $user = User::with('role')
+//            ->where('email', $request->email)
+//            ->first();
 
-        if (empty($user)) {
-            throw new ApiServiceException(400, false,
-                [
-                    'message' => 'Пользователь не зарегистрирован',
-                ]);
-        }
+//        if (empty($user)) {
+//            throw new ApiServiceException(400, false,
+//                [
+//                    'message' => 'Пользователь не зарегистрирован',
+//                ]);
+//        }
 
         if (!($token = $this->guard()->attempt($request->toArray()))) {
             throw new ApiServiceException(400, false,
@@ -36,7 +36,7 @@ class AuthController extends ApiBaseController
 
         return [
             'token' => $token,
-            'user' => new UserResource($user),
+//            'user' => new UserResource($user),
             'expires_in' => $this->guard()->factory()->getTTL() * 60 * 12
         ];
 
